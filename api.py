01d7d0256413b97284babce5f5f8cffde8488bbc
Naveenkumar <naveenkumarchapala686@gmail.com>
2025-07-11 20:30:07 +0530

    news

---
+++ api.py
@@ -0,0 +1,23 @@
+
+from fastapi import FastAPI
+import pickle
+from pydantic import BaseModel
+app=FastAPI()
+with open('models/vector.pkl','rb') as f:
+    vector=pickle.load(f)
+with open('models/model.pkl','rb') as f:
+    model=pickle.load(f)
+
+class base_model(BaseModel):
+    text:str
+
+@app.get('/')
+def root():
+    return 'hey welcome'
+
+@app.post('/predict')
+def predict_re(item:base_model):
+    value=item.text
+    vector_predict=vector.transform([value])
+    model_predict_valuer=model.predict(vector_predict)
+    return (f'News is:',int(model_predict_valuer[0]))
