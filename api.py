e4754807839e2eea97803a84ff780c16e796739e
Naveenkumar <naveenkumarchapala686@gmail.com>
2025-08-20 20:10:50 +0530

    fast api

--- api.py
+++ api.py
@@ -26,7 +26,27 @@ def root():
 @app.post('/predict')
 def predict_re(item:base_model):
     value=item.text
+    ##remove speacial charectors
+    value=re.sub('[^a-z A-Z 0-9]','',value)
+    ##remove stopwords
+    value=" ".join([ wor for wor in value.split() if wor not in stopwords.words('english')])
     
+    ##remove html tags
+    value=BeautifulSoup(value,'lxml').get_text()
+    #remove links
+    value=re.sub(r'(http|https|ftp|ssh)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?','',str(value))
+    ##remove spaces
+    value=" ".join(value.split())
+    ##apply wordlematizing
+   
+    value=" ".join(WordNetLemmatizer().lemmatize(i) for i in value.split())
+    
+
+
     vector_predict=vector.transform([value])
     model_predict_valuer=model.predict(vector_predict)
-    return (f'News is:',int(model_predict_valuer[0]))
+    #return (f'News is:',int(model_predict_valuer[0]))
+    if model_predict_valuer==0:
+        return 'This News is Fack ⚠️☠️🚨'
+    else:
+        return 'This News is Real 😉'
